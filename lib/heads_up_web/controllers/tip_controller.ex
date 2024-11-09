b5ac7f33b213d61ca053e77ddef8511378a2c5dd
Stephen Skubik-Peplaski <codesmith@icloud.com>
2024-11-09 16:01:20 -0500

    Completed Chapter 4

---
+++ lib/heads_up_web/controllers/tip_controller.ex
@@ -0,0 +1,9 @@
+defmodule HeadsUpWeb.TipController do
+  use HeadsUpWeb, :controller
+  alias HeadsUp.Tips
+
+  def index(conn, _params) do
+    tips = Tips.list_tips()
+    render(conn, tips: tips)
+  end
+end
