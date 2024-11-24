4154158bdfe6f39872e8eced268e72262b472bf1
Stephen Skubik-Peplaski <codesmith@icloud.com>
2024-11-24 13:09:15 -0500

    Completed Chapter 9

--- lib/heads_up_web/live/effort_live.ex
+++ lib/heads_up_web/live/effort_live.ex
@@ -4,4 +4,8 @@ defmodule HeadsUpWeb.EffortLive do
   def mount(_params, _session, socket) do
     {:ok, assign(socket, responders: 0, minutes_per_responder: 10)}
   end
+
+  def handle_event("add", %{"quantity" => quantity}, socket) do
+    {:noreply, update(socket, :responders, &(&1 + String.to_integer(quantity)))}
+  end
 end
